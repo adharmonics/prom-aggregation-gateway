92017a904c89f55782e3dc0b89f1eb9ef30e8a92
Marcio Zacarias <mzacarias@everquote.com>
2020-06-23 14:58:41 -0400

    Adding scripts and updating README

---
+++ script/run.sh
@@ -0,0 +1,12 @@
+
+#!/bin/sh
+
+set -e
+
+cd "$(dirname "$0")/.."
+
+echo "==> Cleaning up..."
+docker-compose -f ./docker-compose.yml down --rmi=local --volumes --remove-orphans
+
+echo "==> Running prom-aggregation-gateway..."
+docker-compose -f ./docker-compose.yml up --build
